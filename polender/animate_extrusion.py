6a842749c73b686cf9febed77eb0cc7685bdde27
Anton Goloborodko <goloborodko.anton@gmail.com>
2025-01-15 16:13:52 +0100

    rewrite animate_extrusion as a series of static loop snapshots

--- polender/animate_extrusion.py
+++ polender/animate_extrusion.py
@@ -94,6 +94,16 @@ def make_hooked_chain(
     return obj, hook_empties
 
 
+def change_hook_strength(hooked_objs, new_strength=1.0):
+    # For all objects in scene
+    for obj in hooked_objs:
+    # Check modifiers
+        for mod in obj.modifiers:
+            # If modifier is a hook
+            if mod.type == 'HOOK':
+                mod.strength = new_strength # Change this value (0.0 to 1.0)
+
+
 def add_fiber_softbody(obj):
     # Add soft body modifier first
     soft_body = obj.modifiers.new(name="Softbody", type='SOFT_BODY')
@@ -135,144 +145,183 @@ def add_smooth_skin(obj, skin_radius=0.5):
         skin_vert.radius = (skin_radius, skin_radius)  # (X, Y) radius for each vertex
 
 
-def change_hooks_strength(new_strength):
-    # For all objects in scene
-    for obj in bpy.data.objects:
-    # Check modifiers
-        for mod in obj.modifiers:
-            # If modifier is a hook
-            if mod.type == 'HOOK':
-                mod.strength = new_strength # Change this value (0.0 to 1.0)
-
-
-def _animate_extrusion_threading(
-        hooks,
-        time_span,
-        loop_stem_base_loc,
-        loop_stem_tip_loc,
-        last_stays_at_stem_base=True,
-):
-
-    t_lo, t_hi = time_span
-    frames_per_hook = (t_hi - t_lo) / (len(hooks) + 1)
-
-    for i, hook in enumerate(hooks):
-        # Position 1: Initial position
-        bpy.context.scene.frame_set(int(t_lo))
-        initial_pos = hook.location.copy()
-        hook.location = initial_pos
-        hook.keyframe_insert(data_path="location", frame=int(t_lo))
-
-
-        # Position 2: Loop stem base
-        time_at_loop_stem_base = t_lo + (i+1) * frames_per_hook
-
-        bpy.context.scene.frame_set(int(time_at_loop_stem_base))
-        hook.location = loop_stem_base_loc
-        hook.keyframe_insert(data_path="location", frame=int(time_at_loop_stem_base))
-
-
-        # Position 3: Loop stem tip
-        if last_stays_at_stem_base and i == len(hooks) - 1:
-            continue
-        
-        time_at_loop_stem_tip = time_at_loop_stem_base + frames_per_hook
-        bpy.context.scene.frame_set(int(time_at_loop_stem_tip))
-        hook.location = loop_stem_tip_loc
-        hook.keyframe_insert(data_path="location", frame=int(time_at_loop_stem_tip))
-
-
-def _animate_loop_final_conformation(
-    hooks_loop,
-    t,
-    step,
-    stem_tip_loc,
-    skip_stem=True
-    ):
-
+def _arrange_hooks_into_loop(
+        hooks_loop,
+        step,
+        root_loc, 
+        bridge_width,
+        stem_length=2, 
+        stem_only=False):
+    
     l_loop = len(hooks_loop)
 
-    n_hooks_circle = l_loop - 2
+    real_stem_length = min(stem_length, int(round(l_loop / 2)))
+    # arange hooks of the stem
+    for i in range(real_stem_length):
+        hook = hooks_loop[i]
+        hook.location = root_loc + Vector((-bridge_width/2, i * step, 0))
+
+        hook = hooks_loop[-i-1]
+        hook.location = root_loc + Vector((bridge_width/2, i * step, 0))
+
+
+    if stem_only or l_loop <= 2 * stem_length:
+        return
+
+    n_hooks_circle = l_loop - 2 * stem_length 
     l_circle = n_hooks_circle * step
     r_circle = l_circle / math.pi / 2
-    angle_per_hook = 2 * math.pi / (n_hooks_circle - 1)
-    center_circle = stem_tip_loc + Vector((0, r_circle, 0))
 
+    stem_angle = np.arcsin(bridge_width / 2 / r_circle)
+    loop_total_angle = 2 * math.pi - stem_angle * 2
 
-    for i, hook in enumerate(hooks_loop):
-        # Position 4: final position
-        if skip_stem and (i in [0, 1, len(hooks_loop)-2, len(hooks_loop) - 1]):
-            hook_position = hook.location.copy()
-        
-        else:
-            hook_angle = 1.5 * math.pi - angle_per_hook * (i - 1)
-            hook_position = center_circle.copy()
-            hook_position[0] += r_circle * math.cos(hook_angle)
-            hook_position[1] += r_circle * math.sin(hook_angle)
+    angle_per_hook = loop_total_angle / (n_hooks_circle + 1 )
 
-        bpy.context.scene.frame_set(t)
+    center_circle = root_loc + Vector((0, step * (stem_length - 1), 0)) + Vector((0, r_circle, 0))
+
+    for i, hook in enumerate(hooks_loop[stem_length:-stem_length]):
+        hook_angle = 1.5 * math.pi - angle_per_hook * (i + 1) - stem_angle
+        hook_position = center_circle.copy()
+        hook_position[0] += r_circle * math.cos(hook_angle)
+        hook_position[1] += r_circle * math.sin(hook_angle)
         hook.location = hook_position
+        
+
+def keyframe_hook_loop(
+    t,
+    hooks_loop, 
+    step,
+    root_loc, 
+    bridge_width,
+    stem_length=2, 
+    stem_only=False
+    ):
+
+    bpy.context.scene.frame_set(int(t))
+
+    _arrange_hooks_into_loop(
+        hooks_loop,
+        step,
+        root_loc,
+        bridge_width,
+        stem_length,
+        stem_only)
+    
+    hooks_to_keyframe = hooks_loop[:stem_length] + hooks_loop[-stem_length:] if stem_only else hooks_loop
+
+    for hook in hooks_to_keyframe:    
         hook.keyframe_insert(data_path="location", frame=int(t))
 
 
+
 def _animate_extrusion_no_tails(
-    hooks_loop,
-    time_span = (20, 80),
-    loop_base_width = 2.5,
-    step = 4):
-
-    mid_loop_idx = len(hooks_loop) // 2
-
-    mid_loop_loc = ( hooks_loop[mid_loop_idx].location 
-                   + hooks_loop[mid_loop_idx+1].location) / 2
-                   
-    left_anchor_loc =  mid_loop_loc + Vector((- loop_base_width / 2, 0, 0))
-    right_anchor_loc = mid_loop_loc + Vector((  loop_base_width / 2, 0, 0))
-
-    
-    _animate_extrusion_threading(
-        hooks_loop[0:mid_loop_idx+1][::-1],
+        hooks,        
         time_span,
-        left_anchor_loc,
-        left_anchor_loc + Vector((0, step, 0)),
-    )
-
-    _animate_extrusion_threading(
-        hooks_loop[mid_loop_idx+1:],
-        time_span,
-        right_anchor_loc,
-        right_anchor_loc + Vector((0, step, 0)),
-    )
-    
-    _animate_loop_final_conformation(
-        hooks_loop,
-        time_span[1],
         step,
-        mid_loop_loc + Vector((0, step, 0)),
-        skip_stem=True
-    )
+        bridge_width,
+        stem_length=2,
+        root_loc = None,
+        init_loop_idxs = None,
+):
 
-    return left_anchor_loc, right_anchor_loc
+    if init_loop_idxs is None:
+        init_loop_idxs = (len(hooks) // 2, len(hooks) // 2 + 1)
+
+
+    bpy.context.scene.frame_set(int(time_span[0]))
+    if root_loc is None:
+        root_loc = ( hooks[init_loop_idxs[0]].location 
+                   + hooks[init_loop_idxs[1]].location) / 2
+
+    n_steps = max(init_loop_idxs[0], len(hooks) - init_loop_idxs[1]) + 1
+
+    t_lo, t_hi = time_span
+    frames_per_hook = (t_hi - t_lo) / n_steps
+
+    for i in range(n_steps):
+        t = t_lo + i * frames_per_hook
+        if i == 0:
+            for hook in hooks:
+                hook.keyframe_insert(data_path="location", frame=int(t_lo))
+        else:
+            hooks_to_keyframe = hooks[max(0, init_loop_idxs[0] - i) : min(len(hooks), init_loop_idxs[1] + i)]
+            keyframe_hook_loop(
+                t,
+                hooks_to_keyframe, 
+                step,
+                root_loc, 
+                bridge_width,
+                stem_length=stem_length, 
+                stem_only=False
+                )
+
+
+
+# def _animate_extrusion_no_tails(
+#     hooks_loop,
+#     time_span = (20, 80),
+#     loop_base_width = 2.5,
+#     step = 4):
+
+#     mid_loop_idx = len(hooks_loop) // 2
+
+#     mid_loop_loc = ( hooks_loop[mid_loop_idx].location 
+#                    + hooks_loop[mid_loop_idx+1].location) / 2
+                   
+#     left_anchor_loc =  mid_loop_loc + Vector((- loop_base_width / 2, 0, 0))
+#     right_anchor_loc = mid_loop_loc + Vector((  loop_base_width / 2, 0, 0))
+
+    
+#     _animate_extrusion_threading(
+#         hooks_loop[0:mid_loop_idx+1][::-1],
+#         time_span,
+#         left_anchor_loc,
+#         left_anchor_loc + Vector((0, step, 0)),
+#     )
+
+#     _animate_extrusion_threading(
+#         hooks_loop[mid_loop_idx+1:],
+#         time_span,
+#         right_anchor_loc,
+#         right_anchor_loc + Vector((0, step, 0)),
+#     )
+    
+#     _animate_loop_final_conformation(
+#         hooks_loop,
+#         time_span[1],
+#         step,
+#         mid_loop_loc + Vector((0, step, 0)),
+#         skip_stem=True
+#     )
+
+#     return left_anchor_loc, right_anchor_loc
 
 
 def animate_loop_extrusion(
     hooks,
     loop_span = (30, 60),
     time_span = (20, 80),
-    loop_base_width = 2.5,
+    bridge_width = 2.5,
     step = 4,
     ):
 
 
     # Get all empties with the given prefix, sorted by name
 
+    _animate_extrusion_no_tails(
+            hooks[slice(*loop_span)],        
+            time_span=time_span,
+            step=step,
+            bridge_width=bridge_width,
+            stem_length=2,
+            root_loc = None,
+            init_loop_idxs = None,
+    )
 
-    left_anchor_loc, right_anchor_loc = _animate_extrusion_no_tails(
-        hooks[slice(*loop_span)],
-        time_span = time_span,
-        loop_base_width = loop_base_width,
-        step = step)
-        
+    bpy.context.scene.frame_set(int(time_span[1]))
+    left_anchor_loc = hooks[loop_span[0]].location.copy()
+    right_anchor_loc = hooks[loop_span[1]-1].location.copy()
+
         
     bpy.context.scene.frame_set(int(time_span[0]))
     animate_linear_shift(
@@ -328,53 +377,53 @@ def animate_looparray_extrusion(
             time_span)
 
 
-def animate_resume_extrusion(
-    hooks,
-    prev_loop,
-    new_loop,
-    time_span,
-    step,
-    delay_extrusion=0,
-):
-    # only right-sided extrusion is implemented
+# def animate_resume_extrusion(
+#     hooks,
+#     prev_loop,
+#     new_loop,
+#     time_span,
+#     step,
+#     delay_extrusion=0,
+# ):
+#     # only right-sided extrusion is implemented
     
-    bpy.context.scene.frame_set(time_span[0])
-    prev_loop_locs = (
-        hooks[prev_loop[0]].location.copy(),
-        hooks[prev_loop[1]-1].location.copy()
-        )
-    prev_loop_mid_loc = (prev_loop_locs[0] + prev_loop_locs[1]) / 2
+#     bpy.context.scene.frame_set(time_span[0])
+#     prev_loop_locs = (
+#         hooks[prev_loop[0]].location.copy(),
+#         hooks[prev_loop[1]-1].location.copy()
+#         )
+#     prev_loop_mid_loc = (prev_loop_locs[0] + prev_loop_locs[1]) / 2
 
-    delta = hooks[prev_loop[1] - 1].location - hooks[new_loop[1] - 1].location
+#     delta = hooks[prev_loop[1] - 1].location - hooks[new_loop[1] - 1].location
 
-    extrusion_time_span = (time_span[0] + delay_extrusion, time_span[1])
+#     extrusion_time_span = (time_span[0] + delay_extrusion, time_span[1])
 
-    _animate_extrusion_threading(
-            hooks[prev_loop[1]:new_loop[1]],
-            time_span = extrusion_time_span,
-            loop_stem_base_loc = prev_loop_locs[1],
-            loop_stem_tip_loc = prev_loop_locs[1] + Vector((0, step, 0)),
-            last_stays_at_stem_base=True,
-    )
+#     _animate_extrusion_threading(
+#             hooks[prev_loop[1]:new_loop[1]],
+#             time_span = extrusion_time_span,
+#             loop_stem_base_loc = prev_loop_locs[1],
+#             loop_stem_tip_loc = prev_loop_locs[1] + Vector((0, step, 0)),
+#             last_stays_at_stem_base=True,
+#     )
 
-    animate_linear_shift(
-        hooks[new_loop[1]:], 
-        delta,
-        time_span= extrusion_time_span)
+#     animate_linear_shift(
+#         hooks[new_loop[1]:], 
+#         delta,
+#         time_span= extrusion_time_span)
         
-    _animate_loop_final_conformation(
-        hooks_loop=hooks[slice(*prev_loop)],
-        t=time_span[0],
-        step=step,
-        stem_tip_loc=(prev_loop_mid_loc + Vector((0, step, 0))),
-        skip_stem=True
-    )
+#     _animate_loop_final_conformation(
+#         hooks_loop=hooks[slice(*prev_loop)],
+#         t=time_span[0],
+#         step=step,
+#         stem_tip_loc=(prev_loop_mid_loc + Vector((0, step, 0))),
+#         skip_stem=True
+#     )
 
-    _animate_loop_final_conformation(
-        hooks_loop=hooks[slice(*new_loop)],
-        t=time_span[1],
-        step=step,
-        stem_tip_loc=(prev_loop_mid_loc + Vector((0, step, 0))),
-        skip_stem=True
-    )
+#     _animate_loop_final_conformation(
+#         hooks_loop=hooks[slice(*new_loop)],
+#         t=time_span[1],
+#         step=step,
+#         stem_tip_loc=(prev_loop_mid_loc + Vector((0, step, 0))),
+#         skip_stem=True
+#     )
 
